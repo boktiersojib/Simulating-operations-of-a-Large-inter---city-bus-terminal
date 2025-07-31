2910a62f5ea57ba910b9d67547de5c84b1cd9c5c
boktiersojib <boktiarsojib@gmail.com>
2025-07-31 13:34:05 +0600

    kamkor

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.bus_terminal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.bus_terminal to javafx.fxml;
+    opens mainpkg.bus_terminal.Passenger to javafx.fxml;
+    opens mainpkg.bus_terminal.BusDriver to javafx.fxml;
+    opens mainpkg.bus_terminal.TicketOfficer to javafx.fxml;
+    opens mainpkg.bus_terminal.TerminalManager to javafx.fxml;
+    opens mainpkg.bus_terminal.PaymentSystemAdministrator to javafx.fxml;
+    opens mainpkg.bus_terminal.CustomerSupportAgent to javafx.fxml;
+
+    exports com.example.bus_terminal;
+}
