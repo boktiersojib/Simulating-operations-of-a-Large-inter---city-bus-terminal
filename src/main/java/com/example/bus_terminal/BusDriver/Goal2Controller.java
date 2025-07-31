7d63fe41c6a244fd4805a3eb098eec308e652d19
boktiersojib <boktiarsojib@gmail.com>
2025-08-01 03:04:59 +0600

    sojib

---
+++ src/main/java/com/example/bus_terminal/BusDriver/Goal2Controller.java
@@ -0,0 +1,24 @@
+package com.example.bus_terminal.BusDriver;
+
+public class Goal2Controller
+{
+    @javafx.fxml.FXML
+    private ComboBox SelectTripComboBox;
+    @javafx.fxml.FXML
+    private TableColumn MarkArrivalC2;
+    @javafx.fxml.FXML
+    private RadioButton NoRadioButton;
+    @javafx.fxml.FXML
+    private RadioButton YesRadioButton;
+    @javafx.fxml.FXML
+    private Label MarkArrivalLabel;
+    @javafx.fxml.FXML
+    private TableView TripTableView;
+    @javafx.fxml.FXML
+    private TableColumn TripC1;
+    @javafx.fxml.FXML
+    private Label SelectTripLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/example/bus_terminal/BusDriver/Goal6Controller.java
@@ -0,0 +1,21 @@
+package com.example.bus_terminal.BusDriver;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Goal6Controller
+{
+    @javafx.fxml.FXML
+    private Label notifiaction;
+    @javafx.fxml.FXML
+    private TextField OverTimeScheduleTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void ConfirmovertimeOnAction(ActionEvent actionEvent) {
+    }
+}
